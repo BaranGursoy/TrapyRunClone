ddc1f2544db7db293216a293a4148d0325530dc7
BaranGursoy <barangursoy6@gmail.com>
2021-10-24 20:13:15 +0300

    Player and OogieBoogie added. Also added their and floor's controller

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    private Color lerpedColor = Color.white;
+
+    private MeshRenderer mr;
+    private Rigidbody rb;
+
+    private bool lerpColor = false;
+
+    [SerializeField]
+    private float duration = 0.6f;
+
+    private float t = 0f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.white;
+    }
+
+    private void Update()
+    {
+        if (lerpColor)
+        {
+            t += Time.deltaTime / duration;
+            
+            mr.material.color = lerpedColor;
+            lerpedColor = Color.Lerp(Color.white, Color.red, t);
+            StartCoroutine(StartFloorFall());
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            lerpColor = true;
+        }
+    }
+
+    IEnumerator StartFloorFall()
+    {
+        yield return new WaitForSeconds(0.6f);
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+}
